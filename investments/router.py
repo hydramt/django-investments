695f34f2f595059b19125c7fbb4119eade7cd0fd
Terence Gatt <gatt@hydra.to>
2017-10-16 01:06:48 +0000

    Hashed out app name if statement.

--- investments/router.py
+++ investments/router.py
@@ -1,12 +1,12 @@
 class InvestmentRouter(object):
       def db_for_read(self, model, **hints):
-          if (model._meta.app_label == 'mse'):
+#          if (model._meta.app_label == 'mse'):
 # or (model._meta.app_label == 'homedata'):
              return 'investments'
-          return None
+#          return None
 
       def db_for_write(self, model, **hints):
-          if (model._meta.app_label == 'mse'):
+#          if (model._meta.app_label == 'mse'):
 # or (model._meta.app_label == 'homedata'):
              return 'investments'
-          return None
+#          return None
