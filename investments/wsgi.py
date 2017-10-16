a5ebca2bf2243c97f8d8f62e951fd187144f0405
Terence Gatt <gatt@hydra.to>
2017-10-16 01:16:31 +0000

    Setup wsgi.py

--- investments/wsgi.py
+++ investments/wsgi.py
@@ -7,7 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# add the hellodjango project path into the sys.path
+sys.path.append('/home/nginx/portfolios/investments')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/nginx/portfolios/lib/python2.7/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
